e3519b842a6a975a8cc9b503fa158235257a193c
U-DESKTOP-OJB0GAT\Gautam <gautam.manek@gmail.com>
2021-05-20 00:15:41 +0800

    deployment fixes

--- client/api/build-client.jsx
+++ client/api/build-client.jsx
@@ -4,8 +4,7 @@ export default ({ req }) => {
   if (typeof window === "undefined") {
     // we are on the server
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://www.ticketing-production.xyz/",
       headers: req.headers,
     });
   } else {
