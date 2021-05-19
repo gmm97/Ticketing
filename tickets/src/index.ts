e5c4f111b707385bc7f16be9551c44266f811cd7
U-DESKTOP-OJB0GAT\Gautam <gautam.manek@gmail.com>
2021-05-19 23:36:43 +0800

    changed all services

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,6 +5,7 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 
 const start = async () => {
+  console.log("Tickets is starting!");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined!");
   }
