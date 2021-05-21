72267b2a3f62f0f88695082da4db945c26db6637
U-DESKTOP-OJB0GAT\Gautam <gautam.manek@gmail.com>
2021-05-21 17:38:15 +0800

    added common submodule

---
+++ common/src/events/base-listener.ts
@@ -0,0 +1,55 @@
+import { Message, Stan } from "node-nats-streaming";
+import { Subjects } from "./subjects";
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Listener<T extends Event> {
+  abstract subject: T["subject"];
+  abstract queueGroupName: string;
+  abstract onMessage(data: T["data"], msg: Message): void;
+  protected client: Stan;
+  protected ackWait = 5000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  // setDeliverAllAvailable gets all events emitted in the past
+  // setDurableName keeps track of all events going to this subscription, even if it remains offline for a while
+  // use a queue group to make sure that when all clients to the subscription are disconnected, NATS Streaming will not dump the history.
+  // Also makes sure only one instance from the queue group is sent the event for processing
+  // Let the durable name and the queueGroupName be the same
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (msg: Message) => {
+      console.log(`Message Recieved: ${this.subject} / ${this.queueGroupName}`);
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf8"));
+  }
+}
